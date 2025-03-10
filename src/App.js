8740d6d942dfe8049cfbc905749887444b27da8b
neha03045 <pawarneha3494@gmail.com>
2025-03-10 16:28:52 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
-       
-=======
-          neHaaaa
->>>>>>> new-feature
+          neHaaaa 
         </p>
         <a
           className="App-link"
